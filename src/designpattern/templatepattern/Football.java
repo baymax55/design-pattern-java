e326caf542226696368afd6312faa256f06420a8
1348580915@qq.com <18161931993@163.com>
2019-08-20 10:07:45 +0800

    add Template Pattern

---
+++ src/designpattern/templatepattern/Football.java
@@ -0,0 +1,22 @@
+package designpattern.templatepattern;
+
+/**
+ * @author bx
+ * @date 8/20/2019 9:59 AM
+ */
+public class Football extends Game {
+    @Override
+    void initialize() {
+        System.out.println("Football Game Initialized! Start playing.");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Football Game Started. Enjoy the game!");
+    }
+
+    @Override
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    }
+}
