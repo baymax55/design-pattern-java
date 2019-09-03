1c0b47543ccf9eb499ab0623d896bbe9487dfe74
1348580915@qq.com <18161931993@163.com>
2019-09-03 15:07:48 +0800

    add Service Locator Pattern

---
+++ src/designpattern/servicelocatorpattern/ServiceLocatorPatternDemo.java
@@ -0,0 +1,18 @@
+package designpattern.servicelocatorpattern;
+
+/**
+ * @author bx
+ * @date 9/3/2019 3:01 PM
+ */
+public class ServiceLocatorPatternDemo {
+    public static void main(String[] args) {
+        Service service = ServiceLocator.getService("service1");
+        service.execute();
+        service = ServiceLocator.getService("service2");
+        service.execute();
+        service = ServiceLocator.getService("service1");
+        service.execute();
+        service = ServiceLocator.getService("service2");
+        service.execute();
+    }
+}
