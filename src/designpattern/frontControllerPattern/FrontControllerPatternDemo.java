ef04405fec7bcba5f246c9d690e451ff4c3b7790
bx <18161931993@163.com>
2019-09-16 22:20:37 +0800

    add Front Controller Pattern

---
+++ src/designpattern/frontControllerPattern/FrontControllerPatternDemo.java
@@ -0,0 +1,13 @@
+package designpattern.frontControllerPattern;
+
+/**
+ * @author bx
+ * @date 9/16/2019 10:16 PM
+ */
+public class FrontControllerPatternDemo {
+    public static void main(String[] args) {
+       FrontController frontController= new FrontController();
+       frontController.dispatchRequest("HOME");
+       frontController.dispatchRequest("STUDENT");
+    }
+}
